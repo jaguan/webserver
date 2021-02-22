0dcd19414f2f8393c38d10d5e6c9268bf2491cd5
jaguan <kjaguan@gmail.com>
2021-02-22 23:29:16 +0900

    add app.go(App struct)

---
+++ app.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+// App is ...
+type App struct {
+	Router *mux.Router
+}
+
+// Initialize is ...
+func (a *App) Initialize() {
+	fmt.Println("[APP] Initialize")
+
+	a.Router = mux.NewRouter().StrictSlash(true)
+	a.Router.HandleFunc("/", handler)
+}
+
+// Run is ...
+func (a *App) Run(port int) {
+	fmt.Println("[APP] Run")
+
+	var addr string = ":" + strconv.Itoa(port)
+
+	log.Fatal(http.ListenAndServe(addr, a.Router))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World")
+}
--- main.go
+++ main.go
@@ -1,26 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-
-	"github.com/jaguan/webserver/handler"
-)
-
 func main() {
-	// 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-	// 		w.Write([]byte("Hello World"))
-	// 	})
-
-	// staticHandler := handler.GetStaticHandler()
-	// http.Handle("/", &staticHandler)
-	http.Handle("/", new(handler.StaticHandler))
-
-	fmt.Println("Server Start")
-	http.ListenAndServe(":5000", nil)
-	fmt.Println("Server End")
-}
-
-func init() {
-	fmt.Println("main Start")
+	a := App{}
+	a.Initialize()
+	a.Run(8080)
 }
