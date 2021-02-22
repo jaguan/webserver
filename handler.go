7561b74bfa1b186dd9080063c1b1c65b2c122e83
jaguan <kjaguan@gmail.com>
2021-02-23 00:12:28 +0900

    test simple static file handler

--- handler.go
+++ handler.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
 // IndexHandler is handler function for "/"
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Index")
+	page, _ := ioutil.ReadFile("index.html")
+	w.Write(page)
 }
 
 // HelloHandler is handler function for "/hello"
